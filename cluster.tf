73266f645d314fe0cc16060a9b9bbc5b0456ee51
Sudhanva Narayana <nsudhanva@gmail.com>
2021-12-12 20:22:03 -0500

    Install and Deploy Kubeflow

--- cluster.tf
+++ cluster.tf
@@ -2,14 +2,14 @@ resource "digitalocean_kubernetes_cluster" "k8s-challenge" {
   name   = "do-k8s-challenge"
   region = "nyc1"
   # Grab the latest version slug from `doctl kubernetes options versions`
-  version = "1.21.5-do.0"
+  version = "1.19.15-do.0"
 
   node_pool {
     name       = "default"
-    size       = "s-1vcpu-2gb"
+    size       = "s-4vcpu-8gb"
     auto_scale = true
     min_nodes  = 1
-    max_nodes  = 1
+    max_nodes  = 4
 
     tags = [
       "challenge",
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,20 @@
+output "kubernetes_id" {
+  description = "ID of the cluster"
+  value       = digitalocean_kubernetes_cluster.k8s-challenge.id
+}
+output "kubernetes_host" {
+  description = "The hostname of the API server for the cluster"
+  value       = digitalocean_kubernetes_cluster.k8s-challenge.endpoint
+}
+
+output "kubernetes_urn" {
+  description = "The uniform resource name (URN) for the Kubernetes cluster."
+  value       = digitalocean_kubernetes_cluster.k8s-challenge.urn
+}
+
+output "kubernetes_created" {
+  description = "Created at timestamp for the cluster"
+  value       = digitalocean_kubernetes_cluster.k8s-challenge.created_at
+}
+
 
